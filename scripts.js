8f3e4979a63a08cf7e79310781a3977aaa364e23
Daniel Gracia <danielgraciac@gmail.com>
2016-03-20 15:14:29 -0500

    para pasarte a shirley

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
-console.log("Inicio de programa");
-alert("Hola JavaScript");
-document.write("<h1>Hola JavaScript</h1>");
-alert("Gracias por su Visita");
-console.log("Fin de Programa");
+console.log("pon el punto y coma")
+alert("le falta el parentesis";
+document.write("<h1>cierra el tag");
+aler("es alert");
+consol.log("se escribe console");
+
+console.("pon el log ");
+lert("alert comienza con a");
+Document.write("<h1>Documente es con d minuscula</h1>");
+Document.("<h1>Falta Write</h1>");
+console.log(va entre comillas);
