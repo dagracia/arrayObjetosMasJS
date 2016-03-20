87f141eed8330f95fdde4c77c86c1bb6a5a90cae
Daniel Gracia <danielgraciac@gmail.com>
2016-03-20 17:33:14 -0500

    tarea en js de frases concatenadas

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-console.log("pon el punto y coma")
+/*console.log("pon el punto y coma")
 alert("le falta el parentesis";
 document.write("<h1>cierra el tag");
 aler("es alert");
@@ -8,4 +8,25 @@ console.("pon el log ");
 lert("alert comienza con a");
 Document.write("<h1>Documente es con d minuscula</h1>");
 Document.("<h1>Falta Write</h1>");
-console.log(va entre comillas);
+console.log(va entre comillas);*/
+/*console.log ("Inicio de programa");
+alert("Hola Javascript");
+document.write("<h1>saludo iunicial</h1>");
+alert ("gracias por visitar")
+console.log ("fin de programa");*/
+/*var equipo = prompt("Cual es tu equipo de futbol favorito? ");
+document.write("<h1>Su nombre es "+equipo+"</h1>");
+console.log("Su nombre es "+equipo);*/
+/*var clave = prompt("Hola Usuario, Ingrese su clave:");
+console.log("La longitud de la clave es: " + clave.length);
+console.log("TA-DA!! Su clave es: " + clave.toUpperCase());
+console.log("El valor original de la clave es: " + clave);*/
+var nombre = prompt("ingrese un nombre:");
+var verbo = prompt("ingrese un verbo:");
+var adjetivo = prompt("ingrese un adjetivo:");
+/*document.write("<h1>Mi papá tiene un " +nombre+ " muy "+adjetivo+" y a veces nos lleva a "+verbo+" a mis amigos y a mí</h1>");*/
+
+document.write("<h1>Un día lunes por la mañana llego " + nombre.toUpperCase() + " a la escuela y le dice a su maestra: <br>Maestra, ¿Me va a " + verbo.toUpperCase() 
+	+ " usted por algo que yo no hice? <br>No " + nombre.toUpperCase() + ", ¡Por supuesto que no!  <br>Que " + adjetivo.toUpperCase() + " porque no hice la tarea</h1>");
+
+
